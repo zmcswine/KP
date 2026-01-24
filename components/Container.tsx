2d2f6697f2d6ba4034c3946467c6185e0b23952d
Z Mac <55778389+zmcswine@users.noreply.github.com>
2026-01-24 00:21:14 -0500

    Delete components directory

--- components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export function Container({ children }: PropsWithChildren) {
-  return (
-    <div className="mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8">
-      {children}
-    </div>
-  );
-}
